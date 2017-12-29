2f7dadf956ff54aab028e2469b4c41a27b90d7a9
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-12-29 19:29:53 +0200

    update forms to match functionality

--- src/main/java/sec/project/domain/Account.java
+++ src/main/java/sec/project/domain/Account.java
@@ -17,7 +17,6 @@ public class Account extends AbstractPersistable<Long> {
         this();
         this.name = name;
         this.password = password;
-        System.out.println("Created account with name: " + this.getName() + " password: " + this.getPassword());
     }
 
     public String getName() {
