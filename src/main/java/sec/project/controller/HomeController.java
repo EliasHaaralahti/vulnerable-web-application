dda5da7729aceb44dfb84d9557ba499cfb34e668
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-12-29 19:27:38 +0200

    add basic security, display creator along note

--- src/main/java/sec/project/controller/HomeController.java
+++ src/main/java/sec/project/controller/HomeController.java
@@ -1,36 +1,35 @@
 package sec.project.controller;
 
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import sec.project.domain.Account;
 import sec.project.domain.Note;
-import sec.project.repository.AccountRepository;
 import sec.project.repository.NoteRepository;
 
 @Controller
-public class NoteController {
-
-    @Autowired
-    private AccountRepository accountRepository;
-    
+public class HomeController {
     @Autowired
     private NoteRepository noteRepository;
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String loadForm(Model model) {
-        System.out.println("Accounts found: " + accountRepository.count() );
+    public String loadForm(Model model, HttpSession session) {
+        if (session.getAttribute("loggedUserName") == null) {
+            return "redirect:login";
+        }
+        
+        model.addAttribute("userName", session.getAttribute("loggedUserName"));
         model.addAttribute("notes", noteRepository.findAll() );
         return "home";
     }
 
     @RequestMapping(value = "/home", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String content) {
-        System.out.println("Home: Post: " + content);
-        noteRepository.save( new Note(content) );
+    public String submitForm(HttpSession session, @RequestParam String content) {
+        String userName = session.getAttribute("loggedUserName").toString();
+        noteRepository.save( new Note(content, userName) );
         return "redirect:/home";
     }
 }
