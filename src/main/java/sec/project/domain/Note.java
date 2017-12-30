41db6f5dc1ddcb4cc6319cdc44bdf206f2a75460
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-12-30 14:23:55 +0200

    add title and content to note

--- src/main/java/sec/project/domain/Note.java
+++ src/main/java/sec/project/domain/Note.java
@@ -1,22 +1,27 @@
 package sec.project.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class Note extends AbstractPersistable<Long> {
 
+    @Id @GeneratedValue long id;
     private String content;
     private String creator;
+    private String title;
 
     public Note() {
         super();
     }
 
-    public Note(String name, String creator) {
+    public Note(String title, String content, String creator) {
         this();
-        this.content = name;
+        this.content = content;
         this.creator = creator;
+        this.title = title;
     }
 
     public String getContent() {
@@ -33,5 +38,17 @@ public class Note extends AbstractPersistable<Long> {
     
     public void setCreator(String creator) {
         this.creator = creator;
+    }
+    
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getID() {
+        return Long.toString(this.id);
     }
 }
