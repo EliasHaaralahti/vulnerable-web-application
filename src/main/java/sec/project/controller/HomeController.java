4b5782f6929e60073deb3f88a615ea0bd7ba358d
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-12-30 14:24:15 +0200

    add requestMapping for note/id

--- src/main/java/sec/project/controller/HomeController.java
+++ src/main/java/sec/project/controller/HomeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +28,17 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/home", method = RequestMethod.POST)
-    public String submitForm(HttpSession session, @RequestParam String content) {
+    public String submitForm(HttpSession session, @RequestParam String title,
+                                @RequestParam String content) {
+        
         String userName = session.getAttribute("loggedUserName").toString();
-        noteRepository.save( new Note(content, userName) );
+        noteRepository.save( new Note(title, content, userName) );
         return "redirect:/home";
+    }
+    
+    @RequestMapping(value = "/note/{id}", method = RequestMethod.GET)
+    public String getNote(Model model, @PathVariable(value="id") String id) {
+        model.addAttribute("note", noteRepository.findOne(Long.parseLong(id)) );
+        return "note";
     }
 }
